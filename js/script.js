f9cf1fb8a422fe1411d1c9a08b40d8ba19ea0fb6
francesco <francescosanti91@gmail.com>
2021-10-20 14:45:15 +0200

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// obiettivo: generare una password da assegnare all'utente
+
+// definisco le variabili
+let nome;
+let cognome;
+let colore;
+
+// chiedo all'utente il suo nome
+nome = prompt("Qual è il tuo nome?");
+
+// chiedo all'utente il suo cognome
+cognome = prompt("Qual è il tuo cognome?");
+
+// chiedo all'utente il suo colore preferito
+colore = prompt("Qual è il tuo colore preferito?");
+
+// debug
+/* console.log(
+    `
+    ${nome}
+    ${cognome}
+    ${colore}
+    `
+); */
+
+// stampo a schermo la password assegnata all'utente
+document.getElementById("pwd-generata").innerHTML=`La password assegnata &egrave;: ${nome}${cognome}${colore}21`;
